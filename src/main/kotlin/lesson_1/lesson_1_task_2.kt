775f4a1db43b50e5800f58ba5833ac9038d7cbe9
Ibodov <ibodovparviz@gmail.com>
2023-09-08 22:54:56 +0300

    KS-1-2 solution of the second problem.

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,17 @@
+package lesson_1
+
+fun main() {
+
+    val quantityOrders: Int = 75
+    val thanksForShopping: String = "Благодарим вас за покупки, в подарок Вам конфетка :) "
+
+    println("Количество заказов = $quantityOrders")
+    println(thanksForShopping)
+
+    var numberOfWorkers: Int = 2000
+//    println("Количество работников = $numberOfWorkers")
+    numberOfWorkers = 1999
+
+    println("Количество работников = $numberOfWorkers")
+
+}
