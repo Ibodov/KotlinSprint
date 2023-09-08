3815e3feb5b0b8e0506214fd3019e04ce53ff596
Ibodov <ibodovparviz@gmail.com>
2023-09-08 22:21:03 +0300

    KS-1-1 solution of the first problem.

--- src/main/kotlin/lesson_1/lesson_1_task_1.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_1.kt
@@ -1,5 +1,9 @@
 
 
 fun main() {
-    println("Это решение задачи 1 из урока 1")
+    val quantityOrders: Int = 75
+    val thanksForShopping: String = "Благодарим вас за покупки, в подарок Вам конфетка :) "
+
+    println(thanksForShopping)
+//    println("Это решение задачи 1 из урока 1")
 }
